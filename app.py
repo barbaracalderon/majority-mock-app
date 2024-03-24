8d69d60148ba002efb8c447464b8761078b169f8
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-23 22:00:15 -0300

    feat: create dataframe to use in app

--- app.py
+++ app.py
@@ -1,7 +1,44 @@
-from dash import Dash
-from views import ContentRender
+import pandas as pd
 
-def create_app():
-    app = ContentRender().app
-    return app
+def merge_data():
+    bills_df = pd.read_csv("data/bills.csv")
+    legislators_df = pd.read_csv("data/legislators.csv")
+    votes_df = pd.read_csv("data/votes.csv")
+    vote_results_df = pd.read_csv("data/vote_results.csv")
+
+    vote_results_merged = pd.merge(
+        vote_results_df,
+        legislators_df,
+        left_on="legislator_id",
+        right_on="id",
+        suffixes=("_vote", "_legislator"),
+    )
+
+    vote_results_merged = pd.merge(
+        vote_results_merged,
+        votes_df,
+        left_on="vote_id",
+        right_on="id",
+        suffixes=("_vote_results", "_votes"),
+    )
+
+    vote_results = pd.merge(
+        vote_results_merged,
+        bills_df,
+        left_on="bill_id",
+        right_on="id",
+        suffixes=("_votes", "_bills"),
+    )
+
+    final_table = pd.merge(
+        vote_results,
+        legislators_df[["id", "name"]],
+        left_on="sponsor_id",
+        right_on="id",
+        how="left",
+    )
+
+    final_table.drop(columns=["id_vote", "id_votes", "id_bills"], inplace=True)
+    print(final_table)
+    return final_table
 
